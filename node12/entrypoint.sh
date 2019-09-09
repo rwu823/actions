9d150061d55e4ee619d72dc9895102fbaa4ba6da
Rocky Wu <rocky@17.media>
2019-09-09 14:23:02 +0800

    fix entrypoint

--- node12/entrypoint.sh
+++ node12/entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-bash -c "$@"
+for cmd in "$@"; do
+    echo "$cmd"
+    if sh -c "$cmd"; then
+    else
+        exit_code=$?
+        echo "Failure running '$cmd', exited with $exit_code"
+        exit $exit_code
+    fi
+done
